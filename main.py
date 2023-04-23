a80292dda0c2c1e75eeaf6797a4ec6f176dd0765
K0-M1 <d3vmirko@gmail.com>
2023-04-23 23:20:17 +0200

    changed the default key

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import threading
 from pynput.mouse import Controller, Button
 from pynput.keyboard import Listener, KeyCode
 
-ToggleKey = KeyCode(char="cc")
+ToggleKey = KeyCode(char="-")
 
 isClicking = False
 mouse = Controller()
